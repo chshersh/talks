00c91c07b802581edd5f473c68812d4c4a500bb3
Dmitrii Kovanikov <kovanikov@gmail.com>
2022-10-23 17:36:46 +0100

    Create Free3.hs

---
+++ 2022-11-05-free-monads/code/Free3.hs
@@ -0,0 +1,42 @@
+{-# OPTIONS_GHC -Wall #-}
+
+{-# LANGUAGE OverloadedStrings #-}
+
+module Free2 where
+
+import System.Directory (getDirectoryContents, getCurrentDirectory)
+import Data.Text (Text)
+
+import qualified Data.Text as Text
+import qualified Data.Text.IO as TextIO
+
+data Cmd
+    = Echo [Text] Cmd
+    | Ls ([Text] -> Cmd)
+    | Finish
+
+runCommand :: Cmd -> IO ()
+runCommand cmd = case cmd of
+    Echo args next -> do
+        TextIO.putStrLn $ Text.unwords args
+        runCommand next
+    Ls getNext -> do
+        curDir <- getCurrentDirectory
+        dirs <- getDirectoryContents curDir
+        let dirNames = map Text.pack dirs
+        runCommand $ getNext dirNames
+    Finish -> pure ()
+
+dryRunCommand :: Cmd -> [Text]
+dryRunCommand cmd = case cmd of
+    Echo args next -> Text.unwords ("echo" : args) : dryRunCommand next
+    Ls next -> "ls -1a" : dryRunCommand (next ["dry-run-test-dir"])
+    Finish -> []
+
+exampleScript :: Cmd
+exampleScript
+    = Echo ["pancake", "honey"]
+    $ Ls 
+    $ \dirs -> Echo dirs
+    $ Echo ["Is", "this", "free", "monad?"]
+    Finish
