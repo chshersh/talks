1bc44b4e9a57cb8df6a44e834502b7b89b69ca64
Dmitrii Kovanikov <kovanikov@gmail.com>
2022-10-23 17:35:41 +0100

    Create Free1.hs

---
+++ 2022-11-05-free-monads/code/Free1.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Free1 where
+
+import Data.Text (Text)
+import Data.Foldable (traverse_)
+
+import qualified Data.Text as Text
+import qualified Data.Text.IO as TextIO
+
+data Cmd
+    = Echo [Text]
+
+runCommand :: Cmd -> IO ()
+runCommand cmd = case cmd of
+    Echo args -> TextIO.putStrLn $ Text.unwords args
+
+newtype Script = Script [Cmd]
+
+runScript :: Script -> IO ()
+runScript (Script commands) = traverse_ runCommand commands
+
+runScriptDryRun :: Script -> [Text]
+runScriptDryRun (Script commands) = map fmtCommand commands
+
+fmtCommand :: Cmd -> Text
+fmtCommand cmd = case cmd of
+    Echo args -> Text.unwords $ "echo" : args
+
+exampleScript :: Script
+exampleScript = Script
+    [ Echo ["pancake", "honey"]
+    , Echo ["Is", "this", "free", "monad?"]
+    ]
